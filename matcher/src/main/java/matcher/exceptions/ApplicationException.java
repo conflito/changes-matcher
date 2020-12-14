d2c689a4c88e95682f90f691dc913faca67d5e12
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2020-12-14 19:48:20 +0000

    ApplicationException class and exceptions package

---
+++ matcher/src/main/java/matcher/exceptions/ApplicationException.java
@@ -0,0 +1,14 @@
+package matcher.exceptions;
+
+public class ApplicationException extends Exception {
+
+	private static final long serialVersionUID = -7851331616545234386L;
+
+	public ApplicationException(String message) {
+		super(message);
+	}
+	
+	public ApplicationException(String message, Exception e) {
+		super(message, e);
+	}
+}
