8b4c1b095918c217e97975fecd7bc1a1f0747ed1
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2020-12-14 19:49:02 +0000

    BaseInstance class

---
+++ matcher/src/main/java/matcher/entities/BaseInstance.java
@@ -0,0 +1,19 @@
+package matcher.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseInstance {
+
+	private List<ClassInstance> classInstances;
+	
+	public BaseInstance() {
+		this.classInstances = new ArrayList<>();
+	}
+	
+	public void addClassInstance(ClassInstance classInstance) {
+		this.addClassInstance(classInstance);
+	}
+	
+	
+}
