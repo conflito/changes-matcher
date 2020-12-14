7bf91b9d63f904bdcf47d2b8423a1bd971cc7112
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2020-12-14 10:40:42 +0000

    MethodInvocationInstance class

---
+++ matcher/src/main/java/matcher/entities/MethodInvocationInstance.java
@@ -0,0 +1,16 @@
+package matcher.entities;
+
+public class MethodInvocationInstance {
+
+	private String qualifiedName;
+
+	public MethodInvocationInstance(String qualifiedName) {
+		super();
+		this.qualifiedName = qualifiedName;
+	}
+
+	public String getQualifiedName() {
+		return qualifiedName;
+	}
+	
+}
