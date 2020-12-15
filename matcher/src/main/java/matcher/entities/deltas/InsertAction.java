72ccfce7b44805af19a443ae10b9607a92585aea
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2020-12-15 18:44:40 +0000

    Auxiliary methods and toString()

--- matcher/src/main/java/matcher/entities/deltas/InsertAction.java
+++ matcher/src/main/java/matcher/entities/deltas/InsertAction.java
@@ -1,5 +1,11 @@
 package matcher.entities.deltas;
 
+import matcher.entities.ConstructorInstance;
+import matcher.entities.FieldAccessInstance;
+import matcher.entities.FieldInstance;
+import matcher.entities.MethodInstance;
+import matcher.entities.MethodInvocationInstance;
+
 public class InsertAction extends ActionInstance {
 
 	private Insertable insertedEntity;
@@ -18,5 +24,50 @@ public class InsertAction extends ActionInstance {
 
 	public Holder getHolderEntity() {
 		return holderEntity;
+	}
+	
+	public boolean isMethodInsert() {
+		return insertedEntity instanceof MethodInstance;
+	}
+	
+	public boolean isFieldInsert() {
+		return insertedEntity instanceof FieldInstance;
+	}
+	
+	public boolean isConstructorInsert() {
+		return insertedEntity instanceof ConstructorInstance;
+	}
+	
+	public boolean isInvocationInsert() {
+		return insertedEntity instanceof MethodInvocationInstance;
+	}
+	
+	public boolean isFieldReadInsert() {
+		return insertedEntity instanceof FieldAccessInstance 
+				&& ((FieldAccessInstance)insertedEntity).isFieldRead();
+	}
+	
+	public boolean isFieldWriteInsert() {
+		return insertedEntity instanceof FieldAccessInstance 
+				&& ((FieldAccessInstance)insertedEntity).isFieldWrite();
+	}
+	
+	public String toString() {
+		StringBuilder result = new StringBuilder("insert ");
+		if(isMethodInsert())
+			result.append("method ");
+		if(isFieldInsert())
+			result.append("field ");
+		if(isConstructorInsert())
+			result.append("constructor ");
+		if(isInvocationInsert())
+			result.append("method invocation of ");
+		if(isFieldReadInsert())
+			result.append("field read of ");
+		if(isFieldWriteInsert())
+			result.append("field write of ");
+		result.append(insertedEntity.getQualifiedName() + " in ");
+		result.append(holderEntity.getQualifiedName());
+		return result.toString();
 	}
 }
