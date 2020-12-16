d261c0932d95083b6552375a754063262c23d847
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2020-12-16 17:04:43 +0000

    Rename of UpdateVisibilityAction and more methods

--- matcher/src/main/java/matcher/entities/deltas/VisibilityAction.java
+++ matcher/src/main/java/matcher/entities/deltas/VisibilityAction.java
@@ -2,13 +2,13 @@ package matcher.entities.deltas;
 
 import matcher.entities.Visibility;
 
-public class UpdateVisibilityAction extends ActionInstance {
+public class VisibilityAction extends ActionInstance {
 
 	private Visible entity;
 	private Visibility oldVisibility;
 	private Visibility newVisibility;
 	
-	public UpdateVisibilityAction(Action action, Visible entity, 
+	public VisibilityAction(Action action, Visible entity, 
 			Visibility oldVisibility, Visibility newVisibility) {
 		super(action);
 		this.entity = entity;
@@ -28,5 +28,25 @@ public class UpdateVisibilityAction extends ActionInstance {
 		return newVisibility;
 	}
 	
+	public boolean isVisibilityInsert() {
+		return oldVisibility == null;
+	}
 	
+	public boolean isVisibilityDelete() {
+		return newVisibility == null;
+	}
+	
+	public boolean isVisibilityUpdate() {
+		return !isVisibilityInsert() && !isVisibilityDelete();
+	}
+	
+	public String toString() {
+		StringBuilder result = new StringBuilder();
+		if(isVisibilityUpdate()) {
+			result.append("update visibility of ");
+			result.append(entity.getQualifiedName());
+			result.append(" from " + getOldVisibility() + " to " + getNewVisibility());
+		}
+		return result.toString();
+	}
 }
