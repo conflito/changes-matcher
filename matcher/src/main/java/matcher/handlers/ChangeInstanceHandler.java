57dc1f87abddc41bdc71e223c1d7c532eec730c1
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2020-12-16 18:35:33 +0000

    Completing a bit of the ChangeInstanceHandler class

--- matcher/src/main/java/matcher/handlers/ChangeInstanceHandler.java
+++ matcher/src/main/java/matcher/handlers/ChangeInstanceHandler.java
@@ -3,20 +3,26 @@ package matcher.handlers;
 import java.io.File;
 
 import matcher.entities.BaseInstance;
+import matcher.entities.ChangeInstance;
+import matcher.entities.deltas.DeltaInstance;
 import matcher.exceptions.ApplicationException;
 import matcher.patterns.ConflictPattern;
 
 public class ChangeInstanceHandler {
-		
-	private BaseInstanceHandler pbh;
-	
+
+	private BaseInstanceHandler bih;
+	private DeltaInstanceHandler dih;
+
 	public ChangeInstanceHandler() {
-		this.pbh = new BaseInstanceHandler();
+		this.bih = new BaseInstanceHandler();
+		this.dih = new DeltaInstanceHandler();
 	}
 
-	public void getChangeInstance(File base, File firstVariant, File secondVariant, ConflictPattern cp) 
+	public ChangeInstance getChangeInstance(File base, File firstVariant, File secondVariant, ConflictPattern cp) 
 			throws ApplicationException {
-		//TODO incomplete
-			BaseInstance baseInstance = pbh.getBaseInstance(base, cp);		
+		BaseInstance baseInstance = bih.getBaseInstance(base, cp);
+		DeltaInstance firstDelta = dih.getDeltaInstance(base, firstVariant, cp);
+		DeltaInstance secondDelta = dih.getDeltaInstance(base, secondVariant, cp);
+		return new ChangeInstance(baseInstance, firstDelta, secondDelta);
 	}
 }
