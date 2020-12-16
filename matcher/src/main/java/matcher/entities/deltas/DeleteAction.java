04c662f5fdefbdcdbd94550f76968a64406b15b9
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2020-12-16 10:50:53 +0000

    Utility methods and toString()

--- matcher/src/main/java/matcher/entities/deltas/DeleteAction.java
+++ matcher/src/main/java/matcher/entities/deltas/DeleteAction.java
@@ -1,5 +1,11 @@
 package matcher.entities.deltas;
 
+import matcher.entities.ConstructorInstance;
+import matcher.entities.FieldAccessInstance;
+import matcher.entities.FieldInstance;
+import matcher.entities.MethodInstance;
+import matcher.entities.MethodInvocationInstance;
+
 public class DeleteAction extends ActionInstance {
 
 	private Deletable deletedEntity;
@@ -17,5 +23,50 @@ public class DeleteAction extends ActionInstance {
 
 	public Holder getHolderEntity() {
 		return holderEntity;
-	}	
+	}
+	
+	public boolean isMethodDelete() {
+		return deletedEntity instanceof MethodInstance;
+	}
+	
+	public boolean isFieldDelete() {
+		return deletedEntity instanceof FieldInstance;
+	}
+	
+	public boolean isConstructorDelete() {
+		return deletedEntity instanceof ConstructorInstance;
+	}
+	
+	public boolean isInvocationDelete() {
+		return deletedEntity instanceof MethodInvocationInstance;
+	}
+	
+	public boolean isFieldReadDelete() {
+		return deletedEntity instanceof FieldAccessInstance 
+				&& ((FieldAccessInstance)deletedEntity).isFieldRead();
+	}
+	
+	public boolean isFieldWriteDelete() {
+		return deletedEntity instanceof FieldAccessInstance 
+				&& ((FieldAccessInstance)deletedEntity).isFieldWrite();
+	}
+	
+	public String toString() {
+		StringBuilder result = new StringBuilder("remove ");
+		if(isMethodDelete())
+			result.append("method ");
+		if(isFieldDelete())
+			result.append("field ");
+		if(isConstructorDelete())
+			result.append("constructor ");
+		if(isInvocationDelete())
+			result.append("method invocation of ");
+		if(isFieldReadDelete())
+			result.append("field read of ");
+		if(isFieldWriteDelete())
+			result.append("field write of ");
+		result.append(deletedEntity.getQualifiedName() + " from ");
+		result.append(holderEntity.getQualifiedName());
+		return result.toString();
+	}
 }
