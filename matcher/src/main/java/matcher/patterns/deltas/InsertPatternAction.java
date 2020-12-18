83251e6e0cafe6e275ba0cd6b8b7a605cee86df9
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2020-12-18 11:18:48 +0000

    InsertPatternAction class

---
+++ matcher/src/main/java/matcher/patterns/deltas/InsertPatternAction.java
@@ -0,0 +1,43 @@
+package matcher.patterns.deltas;
+
+import matcher.entities.deltas.Action;
+import matcher.entities.deltas.ActionInstance;
+import matcher.entities.deltas.InsertAction;
+import matcher.patterns.FreeVariable;
+
+public class InsertPatternAction extends ActionPattern {
+
+	private FreeVariable insertedEntity;
+	private FreeVariable holderEntity;
+
+	public InsertPatternAction(Action action, FreeVariable insertedEntity, FreeVariable holderEntity) {
+		super(action);
+		this.insertedEntity = insertedEntity;
+		this.holderEntity = holderEntity;
+	}
+	
+	@Override
+	public boolean filled() {
+		return insertedEntity.hasValue() && holderEntity.hasValue();
+	}
+
+	@Override
+	public boolean matches(ActionInstance action) {
+		return action instanceof InsertAction && filled() && matches((InsertAction)action);
+	}
+	
+	private boolean matches(InsertAction action) {
+		return getAction() == action.getAction() &&
+			   insertedEntity.matches(action.getInsertedEntity().getQualifiedName()) &&
+			   holderEntity.matches(action.getHolderEntity().getQualifiedName());
+	}
+
+	@Override
+	public void setVariableValue(int id, String value) {
+		if(insertedEntity.isId(id))
+			insertedEntity.setValue(value);
+		else if(holderEntity.isId(id))
+			holderEntity.setValue(value);
+	}
+
+}
