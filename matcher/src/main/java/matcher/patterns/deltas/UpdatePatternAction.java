62e9ce5ee7122ab173d59c5048788113594c5ad2
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2020-12-18 11:19:22 +0000

    UpdatePatternAction class

---
+++ matcher/src/main/java/matcher/patterns/deltas/UpdatePatternAction.java
@@ -0,0 +1,38 @@
+package matcher.patterns.deltas;
+
+import matcher.entities.deltas.Action;
+import matcher.entities.deltas.ActionInstance;
+import matcher.entities.deltas.UpdateAction;
+import matcher.patterns.FreeVariable;
+
+public class UpdatePatternAction extends ActionPattern {
+
+	private FreeVariable entity;
+	
+	public UpdatePatternAction(Action action, FreeVariable entity) {
+		super(action);
+		this.entity = entity;
+	}
+
+	@Override
+	public boolean filled() {
+		return entity.hasValue();
+	}
+
+	@Override
+	public boolean matches(ActionInstance action) {
+		return action instanceof UpdateAction && filled() && matches((UpdateAction)action);
+	}
+	
+	private boolean matches(UpdateAction action) {
+		return getAction() == action.getAction() &&
+			   entity.matches(action.getEntity().getQualifiedName());
+	}
+
+	@Override
+	public void setVariableValue(int id, String value) {
+		if(entity.isId(id))
+			entity.setValue(value);
+	}
+
+}
