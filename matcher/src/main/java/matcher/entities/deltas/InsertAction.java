6629e26b26897f19e2cda54421a89809bc3238af
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2020-12-21 15:52:41 +0000

    Fixing class to accomodate division

--- matcher/src/main/java/matcher/entities/deltas/InsertAction.java
+++ matcher/src/main/java/matcher/entities/deltas/InsertAction.java
@@ -1,9 +1,6 @@
 package matcher.entities.deltas;
 
-import matcher.entities.ConstructorInstance;
 import matcher.entities.FieldAccessInstance;
-import matcher.entities.FieldInstance;
-import matcher.entities.MethodInstance;
 import matcher.entities.MethodInvocationInstance;
 
 public class InsertAction extends ActionInstance {
@@ -26,18 +23,6 @@ public class InsertAction extends ActionInstance {
 		return holderEntity;
 	}
 	
-	public boolean isMethodInsert() {
-		return insertedEntity instanceof MethodInstance;
-	}
-	
-	public boolean isFieldInsert() {
-		return insertedEntity instanceof FieldInstance;
-	}
-	
-	public boolean isConstructorInsert() {
-		return insertedEntity instanceof ConstructorInstance;
-	}
-	
 	public boolean isInvocationInsert() {
 		return insertedEntity instanceof MethodInvocationInstance;
 	}
@@ -54,12 +39,6 @@ public class InsertAction extends ActionInstance {
 	
 	public String toString() {
 		StringBuilder result = new StringBuilder("insert ");
-		if(isMethodInsert())
-			result.append("method ");
-		if(isFieldInsert())
-			result.append("field ");
-		if(isConstructorInsert())
-			result.append("constructor ");
 		if(isInvocationInsert())
 			result.append("method invocation of ");
 		if(isFieldReadInsert())
