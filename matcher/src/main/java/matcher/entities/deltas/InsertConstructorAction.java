09dd64ec7350f61a03c9bd2bda2cfe866c40ad08
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2020-12-21 15:50:57 +0000

    Better division for Constructor inserts

---
+++ matcher/src/main/java/matcher/entities/deltas/InsertConstructorAction.java
@@ -0,0 +1,27 @@
+package matcher.entities.deltas;
+
+import matcher.entities.Visibility;
+
+public class InsertConstructorAction extends InsertAction {
+
+	private Visibility visibility;
+
+	public InsertConstructorAction(Insertable insertedEntity, Holder holderEntity, 
+			Visibility visibility) {
+		super(insertedEntity, holderEntity);
+		this.visibility = visibility;
+	}
+	
+	public Visibility getVisibility() {
+		return visibility;
+	}
+	
+	public String toString() {
+		StringBuilder result = new StringBuilder("insert " + visibility.toString().toLowerCase() 
+				+ " constructor ");
+		result.append(getInsertedEntity().getQualifiedName() + " in ");
+		result.append(getHolderEntity().getQualifiedName());
+		return result.toString();
+	}
+
+}
---
+++ matcher/src/main/java/matcher/patterns/deltas/InsertConstructorPatternAction.java
@@ -0,0 +1,32 @@
+package matcher.patterns.deltas;
+
+import matcher.entities.Visibility;
+import matcher.entities.deltas.ActionInstance;
+import matcher.entities.deltas.InsertConstructorAction;
+import matcher.patterns.FreeVariable;
+
+public class InsertConstructorPatternAction extends InsertPatternAction {
+
+	private Visibility visibility;
+
+	public InsertConstructorPatternAction(FreeVariable insertedEntity, FreeVariable holderEntity, 
+			Visibility visibility) {
+		super(insertedEntity, holderEntity);
+		this.visibility = visibility;
+	}
+	
+	@Override
+	public boolean matches(ActionInstance action) {
+		return action instanceof InsertConstructorAction && filled() 
+				&& matches((InsertConstructorAction)action);
+	}
+	
+	private boolean matches(InsertConstructorAction action) {
+		return getAction() == action.getAction() &&
+			   getInsertedEntity().matches(action.getInsertedEntity().getQualifiedName()) &&
+			   getHolderEntity().matches(action.getHolderEntity().getQualifiedName()) &&
+			   (visibility == null || visibility == action.getVisibility());
+	}
+	
+	
+}
