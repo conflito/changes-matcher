c743daef9c5fbb3d8e04be453e00f932088e9114
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2020-12-23 13:51:34 +0000

    InsertFieldAccessAction instance and pattern

---
+++ matcher/src/main/java/matcher/entities/deltas/InsertFieldAccessAction.java
@@ -0,0 +1,27 @@
+package matcher.entities.deltas;
+
+import matcher.entities.FieldAccessType;
+
+public class InsertFieldAccessAction extends InsertAction {
+
+	private FieldAccessType accessType;
+
+	public InsertFieldAccessAction(Insertable insertedEntity, Holder holderEntity, 
+			FieldAccessType accessType) {
+		super(insertedEntity, holderEntity);
+		this.accessType = accessType;
+	}
+	
+	public FieldAccessType getAccessType() {
+		return accessType;
+	}
+	
+	public String toString() {
+		StringBuilder result = new StringBuilder("insert " + accessType.toString().toLowerCase() 
+				+ " of field ");
+		result.append(getInsertedEntity().getQualifiedName() + " in ");
+		result.append(getHolderEntity().getQualifiedName());
+		return result.toString();
+	}
+
+}
---
+++ matcher/src/main/java/matcher/patterns/deltas/InsertFieldAccessPatternAction.java
@@ -0,0 +1,32 @@
+package matcher.patterns.deltas;
+
+import matcher.entities.FieldAccessType;
+import matcher.entities.deltas.ActionInstance;
+import matcher.entities.deltas.InsertAction;
+import matcher.entities.deltas.InsertFieldAccessAction;
+import matcher.patterns.FreeVariable;
+
+public class InsertFieldAccessPatternAction extends InsertPatternAction {
+
+	private FieldAccessType accessType;
+
+	public InsertFieldAccessPatternAction(FreeVariable insertedEntity, FreeVariable holderEntity, 
+			FieldAccessType accessType) {
+		super(insertedEntity, holderEntity);
+		this.accessType = accessType;
+	}
+	
+	@Override
+	public boolean matches(ActionInstance action) {
+		return action instanceof InsertAction && filled() && matches((InsertFieldAccessAction)action);
+	}
+	
+	private boolean matches(InsertFieldAccessAction action) {
+		return getAction() == action.getAction() &&
+			   getInsertedEntity().matches(action.getInsertedEntity().getQualifiedName()) &&
+			   getHolderEntity().matches(action.getHolderEntity().getQualifiedName()) &&
+			   (accessType == null || accessType == action.getAccessType());
+	}
+	
+	
+}
