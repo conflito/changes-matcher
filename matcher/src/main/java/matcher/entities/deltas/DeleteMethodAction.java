f270e5d849287f1e2f501d942d06467aa52bda0b
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2020-12-30 10:22:18 +0000

    Removing unnecessary field compatibles

--- matcher/src/main/java/matcher/entities/deltas/DeleteMethodAction.java
+++ matcher/src/main/java/matcher/entities/deltas/DeleteMethodAction.java
@@ -1,32 +1,19 @@
 package matcher.entities.deltas;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import matcher.entities.Visibility;
 
 public class DeleteMethodAction extends DeleteAction {
 
 	private Visibility visibility;
-	
-	private List<Holder> compatibles;
 
 	public DeleteMethodAction(Deletable deletedEntity, Holder holderEntity, Visibility visibility) {
 		super(deletedEntity, holderEntity);
 		this.visibility = visibility;
-		compatibles = new ArrayList<>();
 	}
 	
 	public Visibility getVisibility() {
 		return visibility;
-	}
-	
-	public List<Holder> getCompatibles() {
-		return compatibles;
-	}
-
-	public void addCompatible(Holder compatible) {
-		compatibles.add(compatible);
 	}
 	
 	public String toString() {
@@ -34,13 +21,6 @@ public class DeleteMethodAction extends DeleteAction {
 				+ " method ");
 		result.append(getDeletedEntityQualifiedName() + " in ");
 		result.append(getHolderEntityQualifiedName());
-		if(!compatibles.isEmpty()) {
-			result.append("\n");
-			for(Holder h: compatibles) {
-				result.append(getDeletedEntityQualifiedName() + " compatible with ");
-				result.append(h.getQualifiedName() + "\n");
-			}			
-		}
 		return result.toString();
 	}
 }
