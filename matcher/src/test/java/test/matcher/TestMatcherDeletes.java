09e1f9d4fc703870e23775b574b091b6a42ca8aa
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2020-12-30 09:53:23 +0000

    Testing the deletion of a field and a method

---
+++ matcher/src/test/java/test/matcher/TestMatcherDeletes.java
@@ -0,0 +1,71 @@
+package test.matcher;
+
+import matcher.entities.ChangeInstance;
+import matcher.entities.Visibility;
+import matcher.exceptions.ApplicationException;
+import matcher.handlers.ChangeInstanceHandler;
+import matcher.handlers.MatchingHandler;
+import matcher.patterns.BasePattern;
+import matcher.patterns.ClassPattern;
+import matcher.patterns.ConflictPattern;
+import matcher.patterns.FieldPattern;
+import matcher.patterns.FreeVariable;
+import matcher.patterns.MethodPattern;
+import matcher.patterns.deltas.DeleteFieldPatternAction;
+import matcher.patterns.deltas.DeleteMethodPatternAction;
+import matcher.patterns.deltas.DeltaPattern;
+import matcher.utils.Pair;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.util.List;
+
+public class TestMatcherDeletes {
+
+	private static final String SRC_FOLDER = "src/test/resources/";
+	private static final String DEL_FIELD_METHOD_FOLDER = "FieldAndMethodDeleteInstance/";
+
+	@Test
+	public void deletePrivateFieldAndPublicMethodTest() throws ApplicationException {
+		File base = new File(SRC_FOLDER + DEL_FIELD_METHOD_FOLDER + "Square.java");
+		File firstVar = new File(SRC_FOLDER + DEL_FIELD_METHOD_FOLDER + "Square01.java");
+		File secondVar = new File(SRC_FOLDER + DEL_FIELD_METHOD_FOLDER + "Square02.java");
+		ConflictPattern cp = getDeletePrivateFieldAndPublicMethodPattern();
+		ChangeInstanceHandler cih = new ChangeInstanceHandler();
+		ChangeInstance ci = cih.getChangeInstance(base, firstVar, secondVar, cp);
+		MatchingHandler mh = new MatchingHandler();
+		List<List<Pair<Integer, String>>> result = mh.matchingAssignments(ci, cp);
+		assertTrue(result.size() == 1, "More than one result for inserting private "
+				+ "field and public method?");
+		List<Pair<Integer,String>> assignments = result.get(0);
+		assertTrue(assignments.size() == 3, "Not 3 assignments with only 3 variables?");
+		assertTrue(assignments.get(0).getFirst() == 0 && 
+				assignments.get(0).getSecond().equals("base.Square"), "Class is not base.Square?");
+		assertTrue(assignments.get(1).getFirst() == 1 && 
+				assignments.get(1).getSecond().equals("base.Square.t"), "Field is not base.Square.t?");
+		assertTrue(assignments.get(2).getFirst() == 2 && 
+				assignments.get(2).getSecond().equals("base.Square.m()"), "Method is not base.Square.m()?");
+	}
+
+	private ConflictPattern getDeletePrivateFieldAndPublicMethodPattern() {
+		FreeVariable classVar = new FreeVariable(0);
+		FreeVariable fieldVar = new FreeVariable(1);
+		FreeVariable methodVar = new FreeVariable(2);
+
+		BasePattern basePattern = new BasePattern();
+		ClassPattern classPattern = new ClassPattern(classVar);
+		FieldPattern fieldPattern = new FieldPattern(fieldVar, Visibility.PRIVATE);
+		MethodPattern methodPattern = new MethodPattern(methodVar, Visibility.PUBLIC);
+		classPattern.addFieldPattern(fieldPattern);
+		classPattern.addMethodPattern(methodPattern);
+		basePattern.addClassPattern(classPattern);
+		DeltaPattern dp1 = new DeltaPattern();
+		DeltaPattern dp2 = new DeltaPattern();
+		dp1.addActionPattern(new DeleteFieldPatternAction(fieldVar, classVar, Visibility.PRIVATE));
+		dp2.addActionPattern(new DeleteMethodPatternAction(methodVar, classVar, Visibility.PUBLIC));
+
+		return new ConflictPattern(basePattern, dp1, dp2);
+	}
+}
