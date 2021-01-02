c76fd2fe74c03cf42578b859301fdd1ac23f8388
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2021-01-02 15:59:18 +0000

    Proper loading of involved files

--- matcher/src/main/java/matcher/handlers/BaseInstanceHandler.java
+++ matcher/src/main/java/matcher/handlers/BaseInstanceHandler.java
@@ -2,7 +2,6 @@ package matcher.handlers;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -12,72 +11,79 @@ import matcher.entities.BaseInstance;
 import matcher.exceptions.ApplicationException;
 import matcher.patterns.ConflictPattern;
 import matcher.processors.ClassProcessor;
+import spoon.Launcher;
 import spoon.compiler.SpoonResource;
 import spoon.compiler.SpoonResourceHelper;
 import spoon.reflect.code.CtInvocation;
 import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtType;
+import spoon.reflect.reference.CtTypeReference;
 import spoon.reflect.visitor.filter.TypeFilter;
 
 public class BaseInstanceHandler {
 		
 	public BaseInstance getBaseInstance(File base, ConflictPattern cp) throws ApplicationException {
 		SpoonResource resource = null;
+		Launcher launcher = new Launcher();
 		try {
 			resource = SpoonResourceHelper.createFile(base);
+			launcher.addInputResource(resource);
 		} catch (FileNotFoundException e) {
 			throw new ApplicationException("Invalid specified file", e);
 		}
 		CtClass<?> changedClass = (CtClass<?>) new AstComparator().getCtType(resource);
+		loadClassTree(changedClass, launcher);
+		loadInvokedClasses(changedClass, launcher);
+		
 		BaseInstance result = new BaseInstance();
-		for(CtClass<?> c: getInvolvedClasses(changedClass)) {
+		for(CtType<?> t: launcher.buildModel().getAllTypes()) {
 			ClassProcessor processor = new ClassProcessor(cp);
-			processor.process(c);
+			processor.process((CtClass<?>)t);
 			result.addClassInstance(processor.getClassInstance());
 		}
+		
 		return result;
 	}
 	
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	private List<CtClass<?>> getInvolvedClasses(CtClass<?> changedClass) throws ApplicationException{
+	private void loadInvokedClasses(CtClass<?> changedClass, Launcher launcher) 
+			throws ApplicationException {
 		List<CtInvocation<?>> invocations = 
 				changedClass.getElements(new TypeFilter(CtInvocation.class));
 		invocations = invocations.stream()
 								 .filter(i -> !i.toString().equals("super()") &&
-									!getInvocationClassQualifiedName(i).equals(changedClass.getQualifiedName()))
+									!getInvocationClassQualifiedName(i)
+										.equals(changedClass.getQualifiedName()))
 								 .collect(Collectors.toList());
-		List<CtClass<?>> result = new ArrayList<>();
-		result.add(changedClass);
-		for(CtInvocation<?> i: invocations) {
-			Optional<CtClass<?>> invoked = getClass(i);
-			if(invoked.isPresent()) {
-				CtClass<?> actualInvoked = invoked.get();
-				if(!actualInvoked.equals(changedClass) &&
-				   !changedClass.getReference().isSubtypeOf(actualInvoked.getReference()) &&
-				   !result.contains(actualInvoked)) {
-					result.add(actualInvoked);
+		for(CtInvocation<?> invocation: invocations) {
+			String simpleName = invocation.getExecutable().getDeclaringType().getSimpleName();
+			Optional<File> srcFile = FileSystemHandler.getInstance().getSrcFile(simpleName + ".java");
+			if(srcFile.isPresent()) {
+				launcher.addInputResource(srcFile.get().getAbsolutePath());
+			}
+		}
+	}
+
+	private void loadClassTree(CtClass<?> changedClass, Launcher launcher) throws ApplicationException {
+		CtTypeReference<?> superClass = changedClass.getSuperclass();
+		if(superClass != null) {
+			Optional<File> superFile = 
+					FileSystemHandler.getInstance().getSrcFile(superClass.getSimpleName() + ".java");
+			if(superFile.isPresent()) {
+				File sf = superFile.get();
+				launcher.addInputResource(sf.getAbsolutePath());
+				try {
+					SpoonResource resource = SpoonResourceHelper.createFile(sf);
+					CtClass<?> sc = (CtClass<?>) new AstComparator().getCtType(resource);
+					loadClassTree(sc, launcher);
+				} catch (FileNotFoundException e) {
+					throw new ApplicationException("Invalid file", e);
 				}
 			}
 		}
-		return result;
 	}
 	
 	private String getInvocationClassQualifiedName(CtInvocation<?> invocation) {
 		return invocation.getTarget().getType().getQualifiedName();
-	}
-	
-	private Optional<CtClass<?>> getClass(CtInvocation<?> invocation) throws ApplicationException{
-		String simpleName = invocation.getExecutable().getDeclaringType().getSimpleName();
-		Optional<File> srcFile = FileSystemHandler.getInstance().getSrcFile(simpleName + ".java");
-		if(srcFile.isPresent()) {
-			SpoonResource resource = null;
-			try {
-				resource = SpoonResourceHelper.createResource(srcFile.get());
-			} catch (FileNotFoundException e) {
-				throw new ApplicationException("Invalid src file", e);
-			}
-			return Optional.ofNullable((CtClass<?>) new AstComparator().getCtType(resource));
-			
-		}
-		return Optional.empty();
 	}
 }
