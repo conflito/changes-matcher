e22439153b80ab1c3328f891e8c663e768d2dd4f
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2021-03-01 15:59:10 +0000

    getDescriptor method and tests

--- matcher/src/main/java/matcher/entities/Type.java
+++ matcher/src/main/java/matcher/entities/Type.java
@@ -29,6 +29,10 @@ public class Type {
 		return typeRef.isArray();
 	}
 	
+	public boolean isVoid() {
+		return typeRef.toString().equals("void");
+	}
+	
 	public Type getArrayType() {
 		CtArrayTypeReference<?> arrayTypeRef = (CtArrayTypeReference<?>) typeRef;
 		Type result = new Type(arrayTypeRef.getComponentType());
@@ -44,6 +48,67 @@ public class Type {
 	
 	public int hashCode() {
 		return typeRef.hashCode();
+	}
+	
+	public String getDescriptor() {
+		if(isVoid())
+			return "V";
+		else if(isPrimitive())
+			return toPrimitiveDescriptor();
+		else if(!isArray()) {
+			return toComplexDescriptor();
+		}
+		else {
+			return toArrayDescriptor();
+		}
+	}
+	
+	private String toArrayDescriptor() {
+		StringBuilder result = new StringBuilder();
+		CtTypeReference<?> t = typeRef;
+		while(t.isArray()) {
+			CtArrayTypeReference<?> arrayTypeRef = (CtArrayTypeReference<?>) t;
+			t = arrayTypeRef.getComponentType();
+			result.append("[");
+		}
+		
+		result.append(new Type(t).getDescriptor());
+		
+		return result.toString();
+	}
+	
+	private String toComplexDescriptor() {
+		return "L" + typeRef.getQualifiedName().replace('.', '/');
+	}
+
+	private String toPrimitiveDescriptor() {
+		String result = null;
+		TypeFactory f = new TypeFactory();
+		if(typeRef.equals(f.INTEGER_PRIMITIVE)) {
+			result = "I";
+		}
+		else if(typeRef.equals(f.BOOLEAN_PRIMITIVE)) {
+			result = "Z";
+		}
+		else if(typeRef.equals(f.BYTE_PRIMITIVE)) {
+			result = "B";
+		}
+		else if(typeRef.equals(f.SHORT_PRIMITIVE)) {
+			result = "S";
+		}
+		else if(typeRef.equals(f.CHARACTER_PRIMITIVE)) {
+			result = "C";
+		}
+		else if(typeRef.equals(f.LONG_PRIMITIVE)) {
+			result = "J";
+		}
+		else if(typeRef.equals(f.FLOAT_PRIMITIVE)) {
+			result = "F";
+		}
+		else if(typeRef.equals(f.DOUBLE_PRIMITIVE)){
+			result = "D";
+		}
+		return result;
 	}
 	
 	public static Type primitiveToWrapper(Type type) {
--- matcher/src/test/java/test/entities/TestType.java
+++ matcher/src/test/java/test/entities/TestType.java
@@ -9,6 +9,17 @@ import spoon.reflect.factory.TypeFactory;
 public class TestType {
 	
 	private TypeFactory factory = new TypeFactory();
+	
+	private Type[] types = {
+			new Type(factory.BOOLEAN_PRIMITIVE),
+			new Type(factory.INTEGER_PRIMITIVE),
+			new Type(factory.BYTE_PRIMITIVE),
+			new Type(factory.SHORT_PRIMITIVE),
+			new Type(factory.CHARACTER_PRIMITIVE),
+			new Type(factory.LONG_PRIMITIVE),
+			new Type(factory.FLOAT_PRIMITIVE),
+			new Type(factory.DOUBLE_PRIMITIVE)
+		};
 
 	@Test
 	public void isSubtypeWithTrueSubtypeTest() {
@@ -38,5 +49,70 @@ public class TestType {
 		Type wrapper = Type.primitiveToWrapper(type);
 		Type expected = null;
 		assertEquals(expected, wrapper, "Number converted to wrapper class?");
+	}
+	
+	@Test
+	public void primitiveTypesDescriptorTest() {
+		String[] expectedValues = {"Z", "I", "B", "S", "C", "J", "F", "D"};
+		for(int i = 0; i < expectedValues.length; i++) {
+			Type primitive = types[i];
+			String expected = expectedValues[i];
+			String result = primitive.getDescriptor();
+			assertEquals(expected, result, "Wrong primitive descritor?");
+		}
+	}
+	
+	@Test
+	public void numberTypeDescriptorTest() {
+		Type type = new Type(factory.get(Number.class).getReference());
+		String expected = "Ljava/lang/Number";
+		String result = type.getDescriptor();
+		assertEquals(expected, result, "Descriptor of type Number is not"
+				+ "Ljava/lang/Number?");
+	}
+	
+	@Test
+	public void objectTypeDescriptorTest() {
+		Type type = new Type(factory.get(Object.class).getReference());
+		String expected = "Ljava/lang/Object";
+		String result = type.getDescriptor();
+		assertEquals(expected, result, "Descriptor of type Object is not"
+				+ "Ljava/lang/Object?");
+	}
+	
+	@Test
+	public void typeTypeDescriptorTest() {
+		Type type = new Type(factory.get(Type.class).getReference());
+		String expected = "Lmatcher/entities/Type";
+		String result = type.getDescriptor();
+		assertEquals(expected, result, "Descriptor of type Type is not"
+				+ "Lmatcher/entities/Type?");
+	}
+	
+	@Test
+	public void doubleArrayDescriptorTest() {
+		Type type = new Type(factory.createArrayReference(factory.DOUBLE_PRIMITIVE));
+		String expected = "[D";
+		String result = type.getDescriptor();
+		assertEquals(expected, result, "Descriptor of double[] is not"
+				+ "[D?");
+	}
+	
+	@Test
+	public void doubleTwoDimensionalArrayDescriptorTest() {
+		Type type = new Type(factory.createArrayReference(factory.DOUBLE_PRIMITIVE, 2));
+		String expected = "[[D";
+		String result = type.getDescriptor();
+		assertEquals(expected, result, "Descriptor of double[][] is not"
+				+ "[[D?");
+	}
+	
+	@Test
+	public void numberArrayDescriptorTest() {
+		Type type = new Type(factory.createArrayReference(factory.get(Number.class).getReference()));
+		String expected = "[Ljava/lang/Number";
+		String result = type.getDescriptor();
+		assertEquals(expected, result, "Descriptor of Number[] is not"
+				+ "[Ljava/lang/Number?");
 	}
 }
