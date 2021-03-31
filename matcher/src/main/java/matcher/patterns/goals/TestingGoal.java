be837255f07dc50d1bf7c2df89df2bda56d6c871
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2021-03-31 12:00:41 +0100

    Alloweing Constructors to be goals to be called

--- matcher/src/main/java/matcher/patterns/goals/TestingGoal.java
+++ matcher/src/main/java/matcher/patterns/goals/TestingGoal.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import matcher.patterns.ClassPattern;
+import matcher.patterns.ConstructorPattern;
 import matcher.patterns.MethodPattern;
 import matcher.utils.Pair;
 
@@ -12,26 +13,39 @@ public class TestingGoal {
 	private ClassPattern targetClass;
 
 	private List<Pair<ClassPattern, MethodPattern>> methodsToCall;
+	
+	private List<Pair<ClassPattern, ConstructorPattern>> constructorsToCall;
 
 	public TestingGoal(ClassPattern targetClass) {
 		super();
 		this.targetClass = targetClass;
 		methodsToCall = new ArrayList<>();
+		constructorsToCall = new ArrayList<>();
 	}
 	
 	public TestingGoal(TestingGoal goal) {
 		super();
 		this.targetClass = new ClassPattern(goal.targetClass);
 		this.methodsToCall = new ArrayList<>();
+		this.constructorsToCall = new ArrayList<>();
 		for(Pair<ClassPattern, MethodPattern> p: goal.methodsToCall) {
 			ClassPattern cp = new ClassPattern(p.getFirst());
 			MethodPattern mp = new MethodPattern(p.getSecond());
 			this.methodsToCall.add(new Pair<>(cp, mp));
 		}
+		for(Pair<ClassPattern, ConstructorPattern> p: goal.constructorsToCall) {
+			ClassPattern cp = new ClassPattern(p.getFirst());
+			ConstructorPattern csp = new ConstructorPattern(p.getSecond());
+			this.constructorsToCall.add(new Pair<>(cp, csp));
+		}
 	}
 	
 	public void addMethodToCall(ClassPattern cp, MethodPattern mp) {
 		this.methodsToCall.add(new Pair<>(cp, mp));
+	}
+	
+	public void addConstructorToCall(ClassPattern cp, ConstructorPattern csp) {
+		this.constructorsToCall.add(new Pair<>(cp, csp));
 	}
 	
 	public String getTargetClass() {
@@ -40,6 +54,14 @@ public class TestingGoal {
 	
 	public List<String> getMethodsToCall() {
 		List<String> result = new ArrayList<>();
+		
+		for(Pair<ClassPattern, ConstructorPattern> p: constructorsToCall) {
+			ClassPattern cp = p.getFirst();
+			ConstructorPattern csp = p.getSecond();
+			
+			result.add(getClassName(cp) + "."
+					+ csp.getLastMatchedConstructorIdentifier());
+		}
 		
 		for(Pair<ClassPattern, MethodPattern> p: methodsToCall) {
 			ClassPattern cp = p.getFirst();
