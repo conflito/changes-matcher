4102183a4feed66d120fbf80943951da11af5275
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2021-05-21 18:26:01 +0100

    Main class to parse command line

--- matcher/src/main/java/matcher/main/ExecuteMatcher.java
+++ matcher/src/main/java/matcher/main/ExecuteMatcher.java
@@ -1,32 +1,161 @@
 package matcher.main;
 
-import java.util.List;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import matcher.Matcher;
 import matcher.exceptions.ApplicationException;
-import matcher.utils.Pair;
+import matcher.handlers.PropertiesHandler;
+
+import org.apache.commons.cli.*;
 
 public class ExecuteMatcher {
 
 	public static void main(String[] args) throws ApplicationException {
-		if(args.length == 4) {
-			String baseFilePath = args[0];
-			String var1FilePath = args[1];
-			String var2FilePath = args[2];
-			String configFilePath = args[3];
-			
-			String[] bases = {baseFilePath};
-			String[] variants1 = {var1FilePath};
-			String[] variants2 = {var2FilePath};
-			
-			long start = System.currentTimeMillis();
-			Matcher matcher = new Matcher(configFilePath);
-			
-			List<List<Pair<Integer, String>>> result = 
-					matcher.matchingAssignments(bases, variants1, variants2);
-			long end = System.currentTimeMillis();
-			System.out.println(result);
-			System.out.println("TIME: " + (end-start));
+		CommandLine cmd = getCommandLine(args);
+		
+		if(cmd.hasOption("ch")) {
+			dumpConfigFileTemplate();
+			System.exit(0);
 		}
+		
+		String baseFilePaths[] = cmd.getOptionValue('b').split(";");
+		String var1FilePaths[] = cmd.getOptionValue("v1").split(";");
+		String var2FilePaths[] = cmd.getOptionValue("v2").split(";");
+		String configFilePath = cmd.getOptionValue('c');
+		
+		Matcher matcher = new Matcher(configFilePath);
+		
+		//TODO call the match method
+	}
+	
+	private static CommandLine getCommandLine(String[] args) {
+		Options options = getOptions();
+		CommandLineParser parser = new DefaultParser();
+		HelpFormatter formatter = new HelpFormatter();
+		CommandLine cmd = null;
+
+		try {
+			cmd = parser.parse(options, args);
+		} catch (ParseException e) {
+			System.out.println(e.getMessage());
+			formatter.printHelp("utility-name", options);
+			System.exit(1);
+		}
+
+		return cmd;
+	}
+	
+	private static Options getOptions() {
+		Options options = new Options();
+		
+		Option base = new Option("b", "base", true, 
+				"Base file paths arg: <path>;<path>;(...)");
+		base.setRequired(true);
+		
+		Option firstVariant = new Option("v1", "variant1", true, 
+				"First variant file paths arg: <path>;<path>;(...)");
+		firstVariant.setRequired(true);
+		
+		Option secondVariant = new Option("v2", "variant2", true, 
+				"Second variant file paths arg: <path>;<path>;(...)");
+		secondVariant.setRequired(true);
+		
+		Option configFile = new Option("c", "config", true, 
+				"Config file path arg");
+		configFile.setRequired(true);
+		
+		Option configFileHelp = new Option("ch", "config_help", false, 
+				"Dumps the template for the config file");
+		
+		options.addOption(base);
+		options.addOption(firstVariant);
+		options.addOption(secondVariant);
+		options.addOption(configFile);
+		options.addOption(configFileHelp);
+		
+		return options;
+	}
+	
+	private static void dumpConfigFileTemplate() {
+		File f = new File("UNSETTLE_config_template.properties");
+		if(!f.exists()) {
+			try {
+				f.createNewFile();
+				BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+				bw.append("######## Required properties ########");
+				bw.newLine();
+				bw.append("# Path to the source directory of the base version");
+				bw.newLine();
+				bw.append(PropertiesHandler.BASE_SRC_DIR_PROPERTY_KEY + 
+						"=some/example/path/to/replace");
+				
+				bw.newLine();
+				bw.newLine();
+				
+				bw.append("# Path to the source directory of the first variant version");
+				bw.newLine();
+				bw.append(PropertiesHandler.VAR1_SRC_DIR_PROPERTY_KEY + 
+						"=some/example/path/to/replace");
+				
+				bw.newLine();
+				bw.newLine();
+				
+				bw.append("# Path to the source directory of the second variant version");
+				bw.newLine();
+				bw.append(PropertiesHandler.VAR2_SRC_DIR_PROPERTY_KEY + 
+						"=some/example/path/to/replace");		
+				
+				bw.newLine();
+				bw.newLine();
+				
+				bw.append("# Path to the classpath directory of the first variant version");
+				bw.newLine();
+				bw.append(PropertiesHandler.VAR1_CP_DIR_PROPERTY_KEY + 
+						"=some/example/path/to/replace");
+				
+				bw.newLine();
+				bw.newLine();
+				
+				bw.append("# Path to the classpath directory of the second variant version");
+				bw.newLine();
+				bw.append(PropertiesHandler.VAR2_CP_DIR_PROPERTY_KEY + 
+						"=some/example/path/to/replace");
+				
+				bw.newLine();
+				bw.newLine();
+				
+				bw.append("# Path to the classpath directory of the merge version");
+				bw.newLine();
+				bw.append(PropertiesHandler.MERGE_CP_DIR_PROPERTY_KEY + 
+						"=some/example/path/to/replace");
+				
+				bw.newLine();
+				bw.newLine();
+				bw.append("######## Optional properties ########");
+				bw.newLine();
+				bw.append("# Threshold value (double) to consider two versions "
+						+ "'different enough' during test generation");
+				bw.newLine();
+				bw.append("#" + PropertiesHandler.DISTANCE_THRESHOLD_KEY + 
+						"=0.05");
+				bw.newLine();
+				bw.newLine();
+				bw.append("# Time budget in seconds (int) for the test generation");
+				bw.newLine();
+				bw.append("#" + PropertiesHandler.TIME_BUDGET_KEY + "=60");
+				
+				bw.close();
+			} catch (IOException e) {
+				System.out.println("Something went wrong creating the file.");
+				System.exit(1);
+			}
+			System.out.println("Template config file created successfully.");
+		}
+		else
+			System.out.println("A file named 'UNSETTLE_config_template.properties' already "
+					+ "exists. Stopping file creation.");
 	}
 }
