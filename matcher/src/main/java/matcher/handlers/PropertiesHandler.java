dae2032ad6ba45d69955fd82328014a0158ae982
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2021-05-21 11:16:51 +0100

    Adding more properties and improving methods

--- matcher/src/main/java/matcher/handlers/PropertiesHandler.java
+++ matcher/src/main/java/matcher/handlers/PropertiesHandler.java
@@ -12,9 +12,23 @@ public class PropertiesHandler {
 
 	private static PropertiesHandler instance;
 	
-	private static final String BASE_SRC_DIR_PROPERTY_KEY = "base.src.dir";
-	private static final String VAR1_SRC_DIR_PROPERTY_KEY = "var1.src.dir";
-	private static final String VAR2_SRC_DIR_PROPERTY_KEY = "var2.src.dir";
+	//Source properties
+	public static final String BASE_SRC_DIR_PROPERTY_KEY = "base.src.dir";
+	public static final String VAR1_SRC_DIR_PROPERTY_KEY = "var1.src.dir";
+	public static final String VAR2_SRC_DIR_PROPERTY_KEY = "var2.src.dir";
+	
+	//Classpath properties
+	public static final String VAR1_CP_DIR_PROPERTY_KEY = "var1.cp.dir";
+	public static final String VAR2_CP_DIR_PROPERTY_KEY = "var2.cp.dir";
+	public static final String MERGE_CP_DIR_PROPERTY_KEY = "merge.cp.dir";
+	
+	//Configurable properties
+	public static final String DISTANCE_THRESHOLD_KEY = "distance.threshold";
+	public static final String TIME_BUDGET_KEY = "time.budget";
+	
+	//Default values for configurable properties
+	private static final double DEFAULT_DISTANCE_THRESHOLD = 0.05d;
+	private static final int DEFAULT_TIME_BUDGET = 60;
 	
 	private Properties prop;
 	
@@ -23,22 +37,86 @@ public class PropertiesHandler {
 			prop = new Properties();
 			prop.load(input);
 		} catch (FileNotFoundException e) {
-			throw new ApplicationException("Invalid properties file", e);
+			throw new ApplicationException("Properties file not found");
 		} catch (IOException e) {
-			throw new ApplicationException("Error reading properties file", e);
+			throw new ApplicationException("Error reading properties file");
 		}
 	}
 	
-	public String getBaseSourceDirPath() {
-		return prop.getProperty(BASE_SRC_DIR_PROPERTY_KEY);
+	public String getBaseSourceDirPath() throws ApplicationException {
+		String result = prop.getProperty(BASE_SRC_DIR_PROPERTY_KEY);
+		if(result == null)
+			throw new ApplicationException("Properties missing base source dir");
+		return result;
 	}
 	
-	public String getFirstVariantSourceDirPath() {
-		return prop.getProperty(VAR1_SRC_DIR_PROPERTY_KEY);
+	public String getFirstVariantSourceDirPath() throws ApplicationException {
+		String result = prop.getProperty(VAR1_SRC_DIR_PROPERTY_KEY);
+		if(result == null)
+			throw new ApplicationException("Properties missing first variant source dir");
+		return result;
 	}
 	
-	public String getSecondVariantSourceDirPath() {
-		return prop.getProperty(VAR2_SRC_DIR_PROPERTY_KEY);
+	public String getSecondVariantSourceDirPath() throws ApplicationException{
+		String result = prop.getProperty(VAR2_SRC_DIR_PROPERTY_KEY);
+		if(result == null)
+			throw new ApplicationException("Properties missing second variant source dir");
+		return result;
+	}
+	
+	public String getFirstVariantClasspathDirPath() throws ApplicationException {
+		String result = prop.getProperty(VAR1_CP_DIR_PROPERTY_KEY);
+		if(result == null)
+			throw new ApplicationException("Properties missing first variant classpath dir");
+		return result;
+	}
+	
+	public String getSecondVariantClasspathDirPath() throws ApplicationException {
+		String result = prop.getProperty(VAR2_CP_DIR_PROPERTY_KEY);
+		if(result == null)
+			throw new ApplicationException("Properties missing second variant classpath dir");
+		return result;
+	}
+	
+	public String getMergeClasspathDirPath() throws ApplicationException {
+		String result = prop.getProperty(MERGE_CP_DIR_PROPERTY_KEY);
+		if(result == null)
+			throw new ApplicationException("Properties missing merge classpath dir");
+		return result;
+	}
+	
+	public double getDistanceThreshold() throws ApplicationException {
+		if(!hasDistanceThreshold())
+			return DEFAULT_DISTANCE_THRESHOLD;
+		double result = 0.0d;
+		try {
+			result = Double.parseDouble(prop.getProperty(DISTANCE_THRESHOLD_KEY));
+		}
+		catch(NumberFormatException e) {
+			throw new ApplicationException("Invalid distance threshold format");
+		}
+		return result;
+	}
+	
+	public int getTimeBudget() throws ApplicationException {
+		if(!hasTimeBudget())
+			return DEFAULT_TIME_BUDGET;
+		int result = 0;
+		try {
+			result = Integer.parseInt(prop.getProperty(DISTANCE_THRESHOLD_KEY));
+		}
+		catch(NumberFormatException e) {
+			throw new ApplicationException("Invalid time budget format");
+		}
+		return result;
+	}
+	
+	private boolean hasDistanceThreshold() {
+		return prop.containsKey(DISTANCE_THRESHOLD_KEY);
+	}
+	
+	private boolean hasTimeBudget() {
+		return prop.containsKey(TIME_BUDGET_KEY);
 	}
 	
 	public static void createInstance(String path) throws ApplicationException {
