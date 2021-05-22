075cdac2603a38d06f63a75ab4cff0ce713ab7dc
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2021-05-22 18:38:45 +0100

    Documentation for InterfaceImplementationInstance

--- matcher/src/main/java/matcher/entities/InterfaceImplementationInstance.java
+++ matcher/src/main/java/matcher/entities/InterfaceImplementationInstance.java
@@ -1,14 +1,29 @@
 package matcher.entities;
 
+/**
+ * A class representing the implementation of an interface in the system's domain
+ * 
+ * @author Nuno Castanho
+ *
+ */
 public class InterfaceImplementationInstance {
 
 	private String name;
 
+	/**
+	 * Creates an instance of InterfaceImplementationInstance
+	 * @param name
+	 * 			the name of the implemented interface
+	 */
 	public InterfaceImplementationInstance(String name) {
 		super();
 		this.name = name;
 	}
 
+	/**
+	 * Get the implemented interface's name
+	 * @return the implemented interface's name
+	 */
 	public String getName() {
 		return name;
 	}
