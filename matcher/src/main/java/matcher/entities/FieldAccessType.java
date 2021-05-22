c864998e52cf74cb2e4879485034fa5770ef2bd1
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2021-05-22 16:58:38 +0100

    Enum descriptions

--- matcher/src/main/java/matcher/entities/FieldAccessType.java
+++ matcher/src/main/java/matcher/entities/FieldAccessType.java
@@ -1,5 +1,11 @@
 package matcher.entities;
 
+/**
+ * Enum for the type of access to a field
+ * 
+ * @author Nuno Castanho
+ *
+ */
 public enum FieldAccessType {
 
 	READ, WRITE;
--- matcher/src/main/java/matcher/entities/Visibility.java
+++ matcher/src/main/java/matcher/entities/Visibility.java
@@ -1,5 +1,11 @@
 package matcher.entities;
 
+/**
+ * Enum for field, constructor and method accessibility
+ * 
+ * @author Nuno Castanho
+ *
+ */
 public enum Visibility {
 
 	PUBLIC, PRIVATE, PROTECTED, PACKAGE;
