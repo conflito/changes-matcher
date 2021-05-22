2685423a6134e5b15e10432584c5bad6cda40261
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2021-05-22 18:38:55 +0100

    Documentation for InterfaceInstance

--- matcher/src/main/java/matcher/entities/InterfaceInstance.java
+++ matcher/src/main/java/matcher/entities/InterfaceInstance.java
@@ -1,18 +1,37 @@
 package matcher.entities;
 
+/**
+ * A class representing an interface in the system's domain
+ * 
+ * @author Nuno Castanho
+ *
+ */
 public class InterfaceInstance {
 
 	private String name;
 
+	/**
+	 * Creates an instance of InterfaceInstance
+	 * @param name
+	 * 			the name of the interface
+	 */
 	public InterfaceInstance(String name) {
 		super();
 		this.name = name;
 	}
 
+	/**
+	 * Get the interface's qualified name
+	 * @return the interface's qualified name
+	 */
 	public String getQualifiedName() {
 		return name;
 	}
 	
+	/**
+	 * Debug toString method for this interface
+	 * @return a string representing this interface
+	 */
 	public String toStringDebug() {
 		return "Interface " + name;
 	}
