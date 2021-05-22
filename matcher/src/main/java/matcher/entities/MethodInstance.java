be8013c7fba375398c8ad2eaafcb8c3e6f736227
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2021-05-22 17:47:01 +0100

    Documentation of MethodInstance

--- matcher/src/main/java/matcher/entities/MethodInstance.java
+++ matcher/src/main/java/matcher/entities/MethodInstance.java
@@ -8,6 +8,12 @@ import java.util.stream.Collectors;
 
 import matcher.entities.deltas.Visible;
 
+/**
+ * A class representing a method in the system's domain
+ * 
+ * @author Nuno Castanho
+ *
+ */
 public class MethodInstance implements Visible {
 
 	private String name;
@@ -22,6 +28,15 @@ public class MethodInstance implements Visible {
 	
 	private List<MethodInstance> directDependencies;
 	
+	/**
+	 * Creates an instance of MethodInstance
+	 * @param name
+	 * 			the method's name
+	 * @param visibility
+	 * 			the method's visibility
+	 * @param returnType
+	 * 			the method's return type
+	 */
 	public MethodInstance(String name, Visibility visibility, Type returnType) {
 		super();
 		this.name = name;
@@ -32,6 +47,17 @@ public class MethodInstance implements Visible {
 		this.directDependencies = new ArrayList<>();
 	}
 
+	/**
+	 * Creates an instance of MethodInstance
+	 * @param name
+	 * 			the method's name
+	 * @param visibility
+	 * 			the method's visibility
+	 * @param returnType
+	 * 			the method's return type
+	 * @param parameters
+	 * 			the method's parameters' types
+	 */
 	public MethodInstance(String name, Visibility visibility, Type returnType, 
 			List<Type> parameters) {
 		super();
@@ -43,26 +69,59 @@ public class MethodInstance implements Visible {
 		this.directDependencies = new ArrayList<>();
 	}
 	
+	/**
+	 * Get this method's direct dependencies
+	 * @return this method's direct dependencies
+	 */
 	public List<MethodInstance> getDirectDependencies(){
 		return directDependencies;
 	}
 	
+	/**
+	 * Adds a direct dependency to this method
+	 * @param m
+	 * 			the new method dependency 
+	 */
 	public void addDirectDependency(MethodInstance m) {
 		directDependencies.add(m);
 	}
 	
+	/**
+	 * Checks if this method accesses any fields
+	 * @return true if this method accesses any fields; false otherwise
+	 */
 	public boolean hasFieldAccesses() {
 		return !fieldAccesses.isEmpty();
 	}
 	
+	/**
+	 * Checks if this method has direct dependencies
+	 * @return true if this method has direct dependencies; false otherwise
+	 */
 	public boolean hasDependencies() {
 		return !directDependencies.isEmpty();
 	}
 	
+	/**
+	 * Checks if this method depends on a method with a given name
+	 * @param methodName
+	 * 			the name of the method to test dependency
+	 * @return true if this method depends (directly or not) to a method with 
+	 * 		the name methodName; false otherwise
+	 */
 	public boolean dependsOn(String methodName) {
 		return dependsOn(methodName, new HashSet<>());
 	}
 	
+	/**
+	 * Auxiliary method to the dependsOn(String) method
+	 * @param methodName
+	 * 			the name of the method to test dependency
+	 * @param visited
+	 * 			names of the methods already visited to avoid loops
+	 * @return true if this method depends (directly or not) to a method with 
+	 * 		the name methodName; false otherwise
+	 */
 	private boolean dependsOn(String methodName, Set<String> visited) {
 		if(!visited.contains(getQualifiedName())) {
 			visited.add(getQualifiedName());
@@ -81,14 +140,29 @@ public class MethodInstance implements Visible {
 		return false;
 	}
 
+	/**
+	 * Get this method's name
+	 * @return this method's name
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Get this method's qualified name (i.e., its signature)
+	 * @return this method's qualified name
+	 */
 	public String getQualifiedName() {
 		return getSimpleSignature();
 	}
 	
+	/**
+	 * Calculates this method's descriptor as per the Java format
+	 * 
+	 * @see <a href="https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html">
+	 * 		https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html</a>
+	 * @return this method's descriptor
+	 */
 	public String getDescriptor() {
 		StringBuilder result = new StringBuilder("(");
 		
@@ -100,10 +174,20 @@ public class MethodInstance implements Visible {
 		return result.toString();
 	}
 	
+	/**
+	 * Get this method's signature
+	 * 
+	 * A method's signature is its name followed by its parameter types
+	 * @return this method's signature
+	 */
 	private String getSimpleSignature() {
 		return getName() + parametersToString();
 	}
 
+	/**
+	 * Calculates the string form for this method's parameters
+	 * @return the string of the parameters of this method
+	 */
 	private String parametersToString() {
 		StringBuilder result = new StringBuilder(getParameters().toString());
 		result.replace(0, 1, "(");
@@ -111,34 +195,71 @@ public class MethodInstance implements Visible {
 		return result.toString();
 	}
 	
+	/**
+	 * Get this method's visibility
+	 * @return this method's visibility
+	 */
 	public Visibility getVisibility() {
 		return visibility;
 	}
 
+	/**
+	 * Get this method's return type
+	 * @return this method's return type
+	 */
 	public Type getReturnType() {
 		return returnType;
 	}
 
+	/**
+	 * Get this method's parameters
+	 * @return thie method's parameters
+	 */
 	public List<Type> getParameters() {
 		return parameters;
 	}
 
+	/**
+	 * Get the field accesses in this method
+	 * @return the fields accesses in thie method
+	 */
 	public List<FieldAccessInstance> getFieldAccesses() {
 		return fieldAccesses;
 	}
 	
+	/**
+	 * Get the qualified names of the accessed fields in this method
+	 * @return the qualified names of the accessed fields in this method
+	 */
 	public List<String> getFieldAccessesQualifiedNames() {
 		return fieldAccesses.stream()
 							.map(FieldAccessInstance::getQualifiedName)
 							.collect(Collectors.toList());
 	}
 
+	/**
+	 * Get the qualified names of the methods invoked in this method
+	 * @return the qualified names of the methods invoked in this method
+	 */
 	public List<String> getInvocationsQualifiedNames() {
 		return directDependencies.stream()
 								  	  .map(MethodInstance::getQualifiedName)
 								  	  .collect(Collectors.toList());
 	}
 	
+	/**
+	 * Checks if this method is compatible with another one
+	 * <br><br>
+	 * A method M is compatible with a methond N if:<br>
+	 * - M and N have the same name <b>and</b><br>
+	 * - M and N have the same number of parameters <b>and</b><br>
+	 * - For 0<=i<=X, where X is the number of parameters,
+	 * the i-th parameter of M is sub-type of the i-th parameter of N  
+	 * 
+	 * @param m
+	 * 			the other method
+	 * @return true if this method is compatible with m; false otherwise
+	 */
 	public boolean isCompatibleWith(MethodInstance m) {
 		if(m == null || !getName().equals(m.getName())) {
 			return false;
@@ -161,10 +282,20 @@ public class MethodInstance implements Visible {
 		return true;
 	}
 	
+	/**
+	 * Adds a parameter's type to this method
+	 * @param type
+	 * 			the type of the parameter
+	 */
 	public void addParameter(Type type) {
 		this.parameters.add(type);
 	}
 	
+	/**
+	 * Adds a field access to this method
+	 * @param access
+	 * 			the field access
+	 */
 	public void addFieldAccess(FieldAccessInstance access) {
 		this.fieldAccesses.add(access);
 	}
