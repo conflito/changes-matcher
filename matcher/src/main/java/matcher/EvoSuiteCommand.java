130f584d16f51eede8bd95615bc67c027e66bc13
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2021-06-03 18:59:49 +0100

    Run in sandbox_mode=off

--- matcher/src/main/java/matcher/EvoSuiteCommand.java
+++ matcher/src/main/java/matcher/EvoSuiteCommand.java
@@ -18,7 +18,7 @@ public class EvoSuiteCommand {
 		    "-Dcover_methods=\"%s\" -Dinstrument_context=true " +
 		    "-Dtest_factory=multi_test -Dregressioncp=\"%s\" " +
 		    "-Dsecond_regressioncp=\"%s\" -Dassertion_strategy=specific " +
-		    "-Dreplace_calls=false -Ddistance_threshold=%f -Dsearch_budget=%d";
+		    "-Dreplace_calls=false -Ddistance_threshold=%f -Dsearch_budget=%d -Dsandbox_mode=off";
 	
 	private final static String METHOD_DELIMITIER = ":";
 	
