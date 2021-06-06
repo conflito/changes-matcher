ddd773035e86e4b0b8d5f22b40c50ae35bb94c56
Nuno Guilherme Nunes Castanho <ncastanho@lasige.di.fc.ul.pt>
2021-06-06 09:07:38 +0100

    Updating processor

--- matcher/src/main/java/matcher/processors/DeleteActionsProcessor.java
+++ matcher/src/main/java/matcher/processors/DeleteActionsProcessor.java
@@ -497,8 +497,7 @@ public class DeleteActionsProcessor extends DeltaProcessor implements CtVisitor{
 
 	@Override
 	public <T> void visitCtTypeAccess(CtTypeAccess<T> typeAccess) {
-		// TODO Auto-generated method stub
-
+		visit(typeAccess);
 	}
 
 	@Override
