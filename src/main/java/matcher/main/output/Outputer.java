35df4853335cbee60229e872924336cc2626c134
Nuno Castanho <fc49518@alunos.fc.ul.pt>
2021-07-21 16:41:36 +0100

    Outputer interface

---
+++ src/main/java/matcher/main/output/Outputer.java
@@ -0,0 +1,13 @@
+package matcher.main.output;
+
+import java.util.List;
+
+import matcher.exceptions.ApplicationException;
+import matcher.utils.Pair;
+
+public interface Outputer {
+
+	public void write(List<List<Pair<Integer, String>>> assignments,
+			List<Pair<String, List<String>>> testingGoals, 
+			List<String> matchedConflicts) throws ApplicationException;
+}
