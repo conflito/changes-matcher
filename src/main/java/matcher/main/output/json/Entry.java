ac0d81220d03db5a147004983c64bf0a85a9ecdf
Nuno Castanho <fc49518@alunos.fc.ul.pt>
2021-07-21 16:44:07 +0100

    JsonBuilder and Entry classes for json output

---
+++ src/main/java/matcher/main/output/json/Entry.java
@@ -0,0 +1,52 @@
+package matcher.main.output.json;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import matcher.utils.Pair;
+
+public class Entry {
+
+	private class AssignmentEntry{
+		private int variable;
+		private String value;
+
+		private AssignmentEntry(int variable, String value) {
+			super();
+			this.variable = variable;
+			this.value = value;
+		}
+	}
+
+	private class GoalEntry{
+		private String targetClass;
+		private List<String> coverMethods;
+
+		private String coverMethodsLine;
+
+		public GoalEntry(String targetClass, List<String> coverMethods) {
+			super();
+			this.targetClass = targetClass;
+			this.coverMethods = coverMethods;
+			this.coverMethodsLine = String.join(":", coverMethods);
+		}
+	}
+
+	private String conflictName;
+	private List<AssignmentEntry> variableAssignments;
+	private GoalEntry testingGoal;
+
+	public Entry(String conflictName, List<Pair<Integer, String>> assignment,
+			Pair<String, List<String>> testingGoal) {
+		this.conflictName = conflictName;
+		this.variableAssignments = new ArrayList<>();
+		for(Pair<Integer, String> pair: assignment) {
+			variableAssignments.add(
+					new AssignmentEntry(pair.getFirst(), pair.getSecond()));
+		}
+
+		this.testingGoal = new GoalEntry(
+				testingGoal.getFirst(), testingGoal.getSecond());
+
+	}
+}
